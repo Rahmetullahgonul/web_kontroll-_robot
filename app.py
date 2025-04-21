b4f425e7c4cfaac76f4d92aba8e61137f1b11ca2
Rahmetullah Gonul <6rahmetullahgonul@gmial.com>
2025-04-22 00:12:24 +0300

    sensorden veri alma blogu da eklendi

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # CORS modülünü ekledik
+from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Tüm domainlerden gelen isteklere izin ver
+CORS(app)
 
 latest_command = None
+latest_status = {
+    "distance": None,
+    "light": None
+}
 
 @app.route("/")
 def home():
-    return "Robot API Çalışıyor!"
+    return "Robot API çalışıyor!"
 
 @app.route("/set-command", methods=["POST"])
 def set_command():
@@ -23,6 +27,18 @@ def get_command():
     cmd = latest_command
     latest_command = None  # komut alındıktan sonra sıfırla
     return jsonify({"command": cmd})
+
+@app.route("/set-status", methods=["POST"])
+def set_status():
+    global latest_status
+    data = request.get_json()
+    latest_status["distance"] = data.get("distance")
+    latest_status["light"] = data.get("light")
+    return jsonify({"status": "OK", "data": latest_status})
+
+@app.route("/status", methods=["GET"])
+def get_status():
+    return jsonify(latest_status)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
